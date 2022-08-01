e1d2d26bf6d74103d850f3724f5e08a1b796f64b
String03 <40239185+String03@users.noreply.github.com>
2022-08-01 13:38:32 -0300

    Add files via upload

---
+++ MetodoAnonimo/MetodoAnonimo/Parametros.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoAnonimo
+{
+    class Parametros
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> F = delegate (int A, int B)
+            {
+                return A + B;
+            };
+
+            F(4, 4);
+            Console.WriteLine(F(4, 4));
+            Console.ReadKey();
+            
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ MetodoAnonimo/MetodoAnonimo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoAnonimo
+{
+    internal class Program
+    {
+        //delegate void ShowMessageDelegate();
+
+        //static void Main(string[] args)
+        //{
+        //    Action E = ShowMessage;
+        //    E();
+
+        //    Action D = delegate ()
+        //    {
+        //        Console.WriteLine("Mensaje Metodo Anonimo");
+        //    };
+        //    D();
+
+        //    ShowMessageDelegate C = delegate ()
+        //    {
+        //        Console.WriteLine("Delegate Metodo Anonimo");
+
+        //    };
+        //    C();
+
+        //    Console.ReadKey();
+        //}
+
+        //static void ShowMessage()
+        //{
+        //    Console.WriteLine("Mensaje");
+        //}
+    }
+}
